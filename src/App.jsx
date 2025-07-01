dcc400d62522759fad7e3ede17bb241c9e49eb8a
HectorSantanaC <hector.santana.gc@gmail.com>
2025-07-01 21:07:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import PokemonList from './components/PokemonList'
+
+function App() {
+  
+  return (
+    <>
+      <h2>Pokemon Project</h2>
+
+      <PokemonList></PokemonList>
+    </>
+  )
+}
+
+export default App
